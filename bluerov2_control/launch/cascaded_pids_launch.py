6237d10e0913492ac4d750d72b51107f218c6a03
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-04-13 20:50:59 +0200

    more stable model

--- bluerov2_control/launch/cascaded_pids_launch.py
+++ bluerov2_control/launch/cascaded_pids_launch.py
@@ -8,13 +8,17 @@ def generate_launch_description():
     sl.declare_arg('namespace', default_value='bluerov2')
     sl.declare_arg('sliders', default_value=True)
     sl.declare_arg('rviz', default_value=True)
+    sl.declare_arg('ranges', default_value=True)
     
     with sl.group(ns=sl.arg('namespace')):
         
         # load body controller anyway
         sl.node('auv_control', 'cascaded_pid', parameters=[sl.find('bluerov2_control', 'cascaded_pid.yaml')],
                 output='screen')
-                
+
+        with sl.group(if_arg='ranges'):
+            sl.node('anchor_msgs', 'pose2ranges', parameters = [sl.find('bluerov2_control', 'anchors.yaml')])
+
         with sl.group(if_arg='sliders'):
             sl.node('slider_publisher', 'slider_publisher', name='pose_control',
                     arguments=[sl.find('auv_control', 'pose_setpoint.yaml')])
