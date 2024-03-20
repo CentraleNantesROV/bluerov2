cd9bef81358eabcea56b3f651839782d6c49927a
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-03-20 18:56:37 +0100

    ping360 ive

--- bluerov2_description/launch/world_launch.py
+++ bluerov2_description/launch/world_launch.py
@@ -4,14 +4,13 @@ from simple_launch import SimpleLauncher
 def generate_launch_description():
     
     sl = SimpleLauncher()
-    
     sl.declare_arg('gui', default_value=True)
 
     with sl.group(if_arg='gui'):
-        sl.gz_launch(f"-r {sl.find('bluerov2_description', 'demo_world.sdf')}")
+        sl.gz_launch(sl.find('bluerov2_description', 'demo_world.sdf'), "-r")
         
     with sl.group(unless_arg='gui'):
-        sl.gz_launch(f"-s -r {sl.find('bluerov2_description', 'demo_world.sdf')}")
+        sl.gz_launch(sl.find('bluerov2_description', 'demo_world.sdf'), "-r -s")
         
     sl.create_gz_clock_bridge()
         
