65db18073c4f57a0ce9eaefd471625b39af6d0b4
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-11-27 16:12:04 +0100

    world can also spawn

--- bluerov2_description/launch/world_launch.py
+++ bluerov2_description/launch/world_launch.py
@@ -5,6 +5,7 @@ def generate_launch_description():
     
     sl = SimpleLauncher()
     sl.declare_arg('gui', default_value=True)
+    sl.declare_arg('spawn', default_value=True)
 
     with sl.group(if_arg='gui'):
         sl.gz_launch(sl.find('bluerov2_description', 'demo_world.sdf'), "-r")
@@ -17,5 +18,8 @@ def generate_launch_description():
                             GazeboBridge.ros2gz)]
         
     sl.create_gz_bridge(bridges)
+
+    with sl.group(if_arg='spawn'):
+        sl.include('bluerov2_description', 'upload_bluerov2_launch.py')
         
     return sl.launch_description()
